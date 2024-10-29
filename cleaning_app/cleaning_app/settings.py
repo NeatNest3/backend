33226efbc749847abc4a87089c076cba3dff230c
ceubie <cras.ceubanks@gmail.com>
2024-10-29 11:06:10 -0400

    deleted 'DATABASES = {}' because it was overwriting the databases variable and negating the local settings import

--- cleaning_app/cleaning_app/settings.py
+++ cleaning_app/cleaning_app/settings.py
@@ -94,8 +94,6 @@ WSGI_APPLICATION = 'cleaning_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

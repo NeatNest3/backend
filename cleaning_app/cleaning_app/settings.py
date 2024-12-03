b356d4102b099b65bf127f476213093b5000eb72
JesmarieVargas <jesmarie669@gmail.com>
2024-12-02 19:55:27 -0500

    changes in settings

--- cleaning_app/cleaning_app/settings.py
+++ cleaning_app/cleaning_app/settings.py
@@ -214,7 +214,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ),                                        
 }
 
 # Default Auth in rest framework (simplejwt) is to integrate AuthO for Authentication on the backend
